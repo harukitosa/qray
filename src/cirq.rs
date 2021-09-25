c2891de3b793fcc54fac26a37182e0ab44cfd9ea
harukitosa <13haruki28@gmail.com>
2021-09-25 20:29:22 +0900

    fix

--- src/cirq.rs
+++ src/cirq.rs
@@ -1,10 +1,36 @@
-#[derive(Default)]
+use ndarray::*;
+
+#[derive(Debug)]
 pub struct Circuit {
-	name: String
+	name: String,
+	n: usize,
+	qubits: ArrayBase<OwnedRepr<f64>, Dim<[usize; 2]>>,
 }
 
-
 impl Circuit {
+	pub fn new() -> Self {
+		let number:i32 = 2;
+		let n:usize = number.pow(2) as usize;
+		let mut qubits = Array::<f64, _>::zeros((1, n).f());
+		qubits[[0, 0]] = 1.0;
+		Self {name: "sample".to_string(), n, qubits}
+	}
+
+	pub fn set(&self, v: Vec<u16>) -> Self {
+		let mut qubits = Array::<f64, _>::zeros((1, self.n).f());
+		let mut vec = v;
+		vec.reverse();
+		let mut idx = 0;
+		let mut x = 1;
+		for item in vec {
+			println!("{}", item);
+			idx += x * item;
+			x *= 2;
+		}
+		qubits[[0, idx as usize]] = 1.0;
+		Self {name: "sample".to_string(), n: self.n, qubits}
+	}
+
 	pub fn get_name(&self) -> &String {
 		&self.name
 	}
